f7ae70a5e0f02f8b729692032c1895d566269887
Ernesto Jiménez <me@ernesto-jimenez.com>
2012-10-13 01:30:32 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Splitpayat::Application.config.secret_token = 'edc5550954041a14c924f3bc629eacdcaaaca2a5e917612cf6533a66d9e87a643705d70ef33def9a9f0e823845893ab9421309f58d25ce0b6244ad55bde21f2a'
