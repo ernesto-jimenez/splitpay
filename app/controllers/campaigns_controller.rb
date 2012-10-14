94d6f551ea8095fc75fa919301fa6482d2b33e06
Ernesto Jiménez <me@ernesto-jimenez.com>
2012-10-14 11:00:44 +0100

    tiny improvements

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,16 +1,20 @@
 class CampaignsController < ApplicationController
   skip_before_filter :require_login, :only => :show
+  include CampaignsHelper
   def show
     @campaign = Campaign.find_by_random_id!(params[:id])
     @payments_completed = @campaign.paid
 
-    @email_data = {:subject => "#{@campaign.title}",
+    @email_data = {:subject => "Money for #{@campaign.title}",
                    :body => <<-EOS
-#{@campaign.user.name} said:
+Hi!
 
-"#{@campaign.message}\"
+I'm collecting the money for #{@campaign.title} (#{
+campaign_amount(@campaign)} per person)
 
 Please go to #{url_for(:action => 'show', :controller => 'campaigns', :only_path => false)} to pay.
+
+#{@campaign.message}
 EOS
                   }
   end
--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,2 +1,7 @@
 module CampaignsHelper
+  include ActionView::Helpers::NumberHelper
+  def campaign_amount(campaign)
+    number_to_currency(campaign.amount,
+                       Currencies.detail(campaign.currency))
+  end
 end
