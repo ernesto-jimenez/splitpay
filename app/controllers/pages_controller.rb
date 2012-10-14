5ae4c7331f83028cecdd73b0692b3075b0df2ea1
Ernesto Jiménez <me@ernesto-jimenez.com>
2012-10-14 18:26:38 +0100

    static screenshot

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,9 @@ class PagesController < ApplicationController
   skip_before_filter :require_login
 
   def home
-    if logged_in?
+    if params[:screenshot]
+      render :screenshot, :layout => false
+    elsif logged_in?
       redirect_to campaigns_path
     end
   end
