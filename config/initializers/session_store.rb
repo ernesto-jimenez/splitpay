b3c38b97f7ff4caa0280afa645135a83f35f986d
Ernesto Jiménez <me@ernesto-jimenez.com>
2012-10-14 11:30:29 +0100

    don't expire sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Splitpayat::Application.config.session_store :cookie_store, key: '_splitpayat_session'
+Splitpayat::Application.config.session_store :cookie_store,
+  key: '_splitpayat_session',
+  expire_after: 1.year
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
